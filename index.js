54943fa4ab0fc76a4e351d05a70231bd21371a76
Eduardo Reis <eduardo4jesus@gmail.com>
2021-03-15 10:10:37 -0400

    Example of Multiple Ways to receive parameters with different REST API Methods

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(morgan('dev'))
 app.use(bodyParser.urlencoded({extended:false}));
 
+
+
 app.get('/', (request, response) => {
   // console.log('Request Received') 
   // response.send('Welcome');
@@ -40,13 +42,135 @@ app.get('/:first_name/:last_name/:age', (request, response) => {
 })
 
 app.get('/id/:user_id', (request, response) => {
+  
+  console.log('##### GET METHOD')
 
   // Query
   console.log('Query Parameters')
   console.log(request.query)
 
   // Route
-  console.log('#### Route Parameters')
+  console.log('Route Parameters')
+  console.log(request.params) 
+
+  // Body
+  console.log('Body Parameters')
+  console.log(request.body)
+
+  response.end()
+})
+
+app.post('/id/:user_id', (request, response) => {
+
+  console.log('#### POST METHOD')
+
+
+  // Query
+  console.log('Query Parameters')
+  console.log(request.query)
+
+  // Route
+  console.log('Route Parameters')
+  console.log(request.params) 
+
+  // Body
+  console.log('Body Parameters')
+  console.log(request.body)
+
+  response.end()
+})
+
+app.put('/id/:user_id', (request, response) => {
+
+  console.log('#### PUT METHOD')
+
+
+  // Query
+  console.log('Query Parameters')
+  console.log(request.query)
+
+  // Route
+  console.log('Route Parameters')
+  console.log(request.params) 
+
+  // Body
+  console.log('Body Parameters')
+  console.log(request.body)
+
+  response.end()
+})
+
+app.patch('/id/:user_id', (request, response) => {
+
+  console.log('#### PATCH METHOD')
+
+
+  // Query
+  console.log('Query Parameters')
+  console.log(request.query)
+
+  // Route
+  console.log('Route Parameters')
+  console.log(request.params) 
+
+  // Body
+  console.log('Body Parameters')
+  console.log(request.body)
+
+  response.end()
+})
+
+app.delete('/id/:user_id', (request, response) => {
+
+  console.log('#### DELETE METHOD')
+
+
+  // Query
+  console.log('Query Parameters')
+  console.log(request.query)
+
+  // Route
+  console.log('Route Parameters')
+  console.log(request.params) 
+
+  // Body
+  console.log('Body Parameters')
+  console.log(request.body)
+
+  response.end()
+})
+
+app.options('/id/:user_id', (request, response) => {
+
+  console.log('#### OPTIONS METHOD')
+
+
+  // Query
+  console.log('Query Parameters')
+  console.log(request.query)
+
+  // Route
+  console.log('Route Parameters')
+  console.log(request.params) 
+
+  // Body
+  console.log('Body Parameters')
+  console.log(request.body)
+
+  response.end()
+})
+
+app.head('/id/:user_id', (request, response) => {
+
+  console.log('#### HEAD METHOD')
+
+
+  // Query
+  console.log('Query Parameters')
+  console.log(request.query)
+
+  // Route
+  console.log('Route Parameters')
   console.log(request.params) 
 
   // Body
